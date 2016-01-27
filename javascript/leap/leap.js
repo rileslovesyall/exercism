b888c63a3e7b330949edbaee724232fff0c21447
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-26 16:53:25 -0800

    refactored leap.js to make the logic more concise

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,15 +3,14 @@ var Year = function (input) {
 };
 
 Year.prototype.isLeap = function () {
-  if (year % 400 === 0) {
-    return true;
-  } else if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
-    return true;
-  } else {
-    return false;
+  var leap = false;
+  if (year % 4 === 0) {
+    leap = true;
+    if (year % 100 === 0 && year % 400 !== 0){
+      leap = false;
+    }
   }
+  return leap
 };
 
 module.exports = Year;
